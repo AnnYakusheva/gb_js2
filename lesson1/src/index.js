a541f0846d50341aed22c6c32647876748a1d614
Anna Yakusheva <annyakusheva0305070912@gmail.com>
2022-01-01 23:27:42 +0700

    Scss addition

--- lesson1/src/index.js
+++ lesson1/src/index.js
@@ -1,8 +1,8 @@
 import getProductList from "./mock/data";
 import renderGoodsList from "./showcase";
+import './styles/main.scss'
 
-
-const productList = getProductList(20);
+const productList = getProductList(10);
 
 
 renderGoodsList(productList);
