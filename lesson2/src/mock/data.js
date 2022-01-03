6a198200e2c4f7da9fd126f035d3ccd1918eebad
Anna Yakusheva <annyakusheva0305070912@gmail.com>
2022-01-03 21:42:04 +0700

    Lesson2

---
+++ lesson2/src/mock/data.js
@@ -0,0 +1,47 @@
+import {random} from 'lodash'
+
+const PRODUCTS = [
+    'Shirt', 
+    'Shoes',
+    'Hat',
+    'Jacket',
+    'Socks'
+]
+
+const COLORS = [
+    'Green',
+    'Red',
+    'Black',
+    'White',
+    'Gray'
+]
+
+let lastIndex = 0;
+
+function getProductName() {
+    const product = PRODUCTS[random(0, PRODUCTS.length-1)]
+    const colors = COLORS[random(0, COLORS.length-1)]
+
+    return `${product} ${colors}`;
+}
+
+function getProduct() {
+    return {
+        id: ++lastIndex,
+        title: getProductName(),
+        price: random(99, 999)
+    }
+}
+
+function getProductList(count) {
+    let products = [];
+
+    for (let i=0; i<count; i++){
+        products.push(getProduct());
+    }
+
+    console.log(JSON.stringify(products));
+    return products
+}
+
+export default getProductList;
